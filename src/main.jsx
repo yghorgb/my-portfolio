8774edab9661981d3ccea62613dfe3a2f3e50382
Yghor Gabriel <yghor.ag@hotmail.com>
2024-10-28 16:59:04 -0300

    Created Global style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Home } from "./pages/Home";
+import GlobalStyle from "../styles/global";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
     <Home />
   </StrictMode>
 );
